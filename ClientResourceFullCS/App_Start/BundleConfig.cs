2de3fd4da621f803109c01334aca8eb4bd7f490b
David Melinosky <dmelinosky@gmail.com>
2020-05-09 12:15:41 -0400

    Add project files.

---
+++ ClientResourceFullCS/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ClientResourceFullCS
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/wwwroot/js/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/wwwroot/js/jquery.validate.min.js",
+                        "~/wwwroot/js/jquery.validate.unobtrusive.min.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/wwwroot/js/modernizr.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/wwwroot/lib/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/wwwroot/lib/bootstrap/css/bootstrap.min.css",
+                      "~/wwwroot/css/site.css"));
+        }
+    }
+}
---
+++ ClientResourceFullCS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientResourceFullCS.MvcApplication" Language="C#" %>
