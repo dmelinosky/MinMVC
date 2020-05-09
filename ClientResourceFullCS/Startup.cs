121d0615d73b9613c1eca153b4d6ce3c14a45bfa
David Melinosky <dmelinosky@gmail.com>
2020-05-09 12:23:28 -0400

    remove App_Start, move to owin startup

--- ClientResourceFullCS/Startup.cs
+++ ClientResourceFullCS/Startup.cs
@@ -1,11 +1,28 @@
-﻿using System.Web;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ClientResourceFullCS.Startup))]
 
 namespace ClientResourceFullCS
 {
-    public class BundleConfig
+    public class Startup
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+            RegisterBundles(BundleTable.Bundles);
+        }
+
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -26,6 +43,22 @@ namespace ClientResourceFullCS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/wwwroot/lib/bootstrap/css/bootstrap.min.css",
                       "~/wwwroot/css/site.css"));
+        }
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
